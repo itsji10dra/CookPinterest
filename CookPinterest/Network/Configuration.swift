f9219c59b4aa33f1e4cdafc448bbfbde0335a34c
Jitendra <jitendra@cellpointmobile.com>
2018-04-15 16:25:39 +0530

    Updated Configuration...

--- CookPinterest/Network/Configuration.swift
+++ CookPinterest/Network/Configuration.swift
@@ -12,9 +12,18 @@ struct Configuration {
     
     static let clientId     = "4960611306886413734"
     
-    static let secretKey    = "f0ed4725724832345c8dce82cdc05bad2be26e71cceb073489d386ee4972be15"
-    
     static let scope        = "read_public,read_relationships"
     
     static let status       = "XyjSoa42793PkeGuwnUb87h"       //Used in Authentication API, just a random string.
+    
+    static func checkConfiguration() {
+        
+        if clientId.isEmpty || scope.isEmpty {
+            fatalError("""
+                Invalid configuration found.
+                Replace `clientId` as received from developers.pinterest.com.
+                Also, define your `scope`.
+            """)
+        }
+    }
 }
